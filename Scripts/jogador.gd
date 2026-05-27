139bc01ab3d99677da374939687f0bcd7295fadb
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2026-05-27 16:04:41 -0300

    Movimento Grid

--- Scripts/jogador.gd
+++ Scripts/jogador.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
 var tamanho_tile = 16
-var duracao_movimento = 0.5
+@export var duracao_movimento = 0.1
 
-var direcao = Vector2.ZERO
+# Converter as variáveis de Vector2 para Vector2i
+var direcao := Vector2i.ZERO
 var movendo = false
 
+# Acessar o TileMapLayer e a posição no tabuleiro do jogador
 @export var grade_fase : TileMapLayer
+var posicao_grid = Vector2i.ZERO
 
+func _ready() -> void:
+	# Pegamos qual casa mais próxima do centro do jogador e depois movemos o jogador para a posição especifica dessa casa
+	posicao_grid = grade_fase.local_to_map(position)
+	position = grade_fase.map_to_local(posicao_grid)
+	
 func _physics_process(delta: float) -> void:
 	
 	mover_grid()
@@ -21,13 +29,30 @@ func mover_grid():
 		
 	direcao = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 		
-	if direcao == Vector2.ZERO:
+	if direcao == Vector2i.ZERO:
 		return
 	
 	if direcao.x != 0 and direcao.y != 0:
-		direcao = Vector2(sign(direcao.x), 0)
+		direcao = Vector2i(sign(direcao.x), 0)
 		
-	var posicao_final = position + (direcao*tamanho_tile)
+	# Onde o jogador está agora
+	posicao_grid = grade_fase.local_to_map(position)
+	
+	# Calcular para onde ele vai se mover
+	var coordenadas_final = posicao_grid + direcao
+	
+	# Coletar o bloco para onde estamos nos movendo
+	var proximo_bloco = grade_fase.get_cell_tile_data(coordenadas_final)
+	
+	# Se o proximo bloco for nulo, encerrar a função
+	if proximo_bloco == null:
+		return
+	
+	# Se o proximo bloco for Blocked, encerrar a função
+	if proximo_bloco.get_custom_data("Blocked") == true:
+		return
+	
+	var posicao_final = grade_fase.map_to_local(coordenadas_final)
 	
 	var movimento_tween = create_tween()
 	
